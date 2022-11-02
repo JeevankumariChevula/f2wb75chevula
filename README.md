8926d49e24a0663085580444fe46de0e917dc288
JeevankumariChevula <S549475@NWMISSOURI.EDU>
2022-11-02 16:29:32 -0500

    final

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 # Jeevan Kumari Chevula
 Welcome to Lab10
 [Link to Render](https://f2wb75chevula.onrender.com)
+
 Class Name : Earpods
+
 Attributes : Earp_Name(String), Earp_Type(String), Earp_Size(number), Earp_Price(number)
--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var EarpRouter = require('./routes/Earpods');
 var gridBuilderRouter = require('./routes/gridbuild');
-//var selectorRouter = require('./routes/selector');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Earpods',EarpRouter);
 app.use('/gridBuild',gridBuilderRouter);
-//app.use('/selector',selectorRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
